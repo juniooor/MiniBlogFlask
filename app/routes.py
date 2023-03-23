2301bc241058c2b55c0c1325718906f9f5e2d6dd
Juniooor <juniormodern@hotmail.com>
2023-03-22 22:04:33 -0300

    colocando templates no codigo

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,10 @@
+from flask import render_template
+
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World! meu consagrado"
+    user = {'username': 'Junioor'}
+    return render_template('index.html', title='home', user=user)
