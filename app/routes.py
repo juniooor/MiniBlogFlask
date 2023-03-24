e26cf7ea9937d76c086716dee3d8ff585f8490e9
Juniooor <juniormodern@hotmail.com>
2023-03-23 22:32:27 -0300

    usando o jinja2

--- app/routes.py
+++ app/routes.py
@@ -7,4 +7,14 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'Junioor'}
-    return render_template('index.html', title='home', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
